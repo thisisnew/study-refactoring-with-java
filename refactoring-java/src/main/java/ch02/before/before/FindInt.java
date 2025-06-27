112ab44afb586660b57429ef7a3ce618f9bd7405
thisisnew <thisisnew@naver.com>
2025-06-27 20:19:25 +0900

    ch2 리팩토링 전

---
+++ refactoring-java/src/main/java/ch02/before/before/FindInt.java
@@ -0,0 +1,15 @@
+package ch02.before.before;
+
+public class FindInt {
+
+    public static boolean find(int[] data, int target) {
+        boolean flag = false;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                flag = true;
+            }
+        }
+
+        return flag;
+    }
+}
