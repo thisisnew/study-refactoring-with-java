50d5108162aaa79ae2abd52105a5ffc573435b32
thisisnew <thisisnew@naver.com>
2025-06-27 20:08:14 +0900

    ch1 리팩토링 전

---
+++ refactoring-java/src/main/java/ch01/before/Main.java
@@ -0,0 +1,10 @@
+package ch01.before;
+
+public class Main {
+    public static void main(String[] args) {
+        Robot robot = new Robot("Robert");
+        robot.order(0);
+        robot.order(1);
+        robot.order(2);
+    }
+}
---
+++ refactoring-java/src/main/java/ch01/before/Robot.java
@@ -0,0 +1,22 @@
+package ch01.before;
+
+public class Robot {
+    private final String _name;
+
+    public Robot(String name) {
+        _name = name;
+    }
+
+    public void order(int command) {
+        if (command == 0) {
+            System.out.println(_name + " walks.");
+        } else if (command == 1) {
+            System.out.println(_name + " stops.");
+        } else if (command == 2) {
+            System.out.println(_name + " jumps.");
+        } else {
+            System.out.println("Command error. command = " + command);
+        }
+    }
+
+}
