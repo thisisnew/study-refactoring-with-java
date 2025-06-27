017ed4c2b9a8d13ff1309bda191dba4e7602eceb
thisisnew <thisisnew@naver.com>
2025-06-27 20:10:26 +0900

    ch1 첫번쨰 리팩토링

---
+++ refactoring-java/src/main/java/ch01/after/Main.java
@@ -0,0 +1,10 @@
+package ch01.after;
+
+public class Main {
+    public static void main(String[] args) {
+        Robot robot = new Robot("Robert");
+        robot.order(Robot.WALK);
+        robot.order(Robot.STOP);
+        robot.order(Robot.JUMP);
+    }
+}
---
+++ refactoring-java/src/main/java/ch01/after/Robot.java
@@ -0,0 +1,27 @@
+package ch01.after;
+
+public class Robot {
+
+    public static final int WALK = 0;
+    public static final int STOP = 1;
+    public static final int JUMP = 2;
+
+    private final String _name;
+
+    public Robot(String name) {
+        _name = name;
+    }
+
+    public void order(int command) {
+        if (command == WALK) {
+            System.out.println(_name + " walks.");
+        } else if (command == STOP) {
+            System.out.println(_name + " stops.");
+        } else if (command == JUMP) {
+            System.out.println(_name + " jumps.");
+        } else {
+            System.out.println("Command error. command = " + command);
+        }
+    }
+
+}
