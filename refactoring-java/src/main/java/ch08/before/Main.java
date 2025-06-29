d13e129a9627b747349b201cae5579dc6948f7bb
thisisnew <thisisnew@naver.com>
2025-06-29 15:45:32 +0900

    ch8 리팩토링 전

---
+++ refactoring-java/src/main/java/ch08/before/Main.java
@@ -0,0 +1,15 @@
+package ch08.before;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape line = new Shape(Shape.TYPECODE_LINE, 0, 0, 100, 200);
+        Shape rectangle = new Shape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40);
+        Shape oval = new Shape(Shape.TYPECODE_OVAL, 100, 200, 300, 400);
+
+        Shape[] shapes = { line, rectangle, oval };
+
+        for (Shape shape : shapes) {
+            shape.draw();
+        }
+    }
+}
