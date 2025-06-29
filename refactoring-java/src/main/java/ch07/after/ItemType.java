50a27f517dbfbe9ee982bd4f235d0e74650c8ad3
thisisnew <thisisnew@naver.com>
2025-06-29 15:35:34 +0900

    ch7 두번째 리팩토링

--- refactoring-java/src/main/java/ch07/after/ItemType.java
+++ refactoring-java/src/main/java/ch07/after/ItemType.java
@@ -1,9 +1,9 @@
 package ch07.after;
 
-public class ItemType {
-    public static final ItemType BOOK = new ItemType(0);
-    public static final ItemType DVD = new ItemType(1);
-    public static final ItemType SOFTWARE = new ItemType(2);
+public enum ItemType {
+    BOOK(0),
+    DVD(1),
+    SOFTWARE(2);
 
     private final int _typeCode;
 
