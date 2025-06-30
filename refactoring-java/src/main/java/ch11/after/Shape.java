5b461d0678a31de4ea04c6cfcc2066bcb11cfa10
thisisnew <thisisnew@naver.com>
2025-06-30 21:40:33 +0900

    ch11 첫번째 리팩토링

---
+++ refactoring-java/src/main/java/ch11/after/Shape.java
@@ -0,0 +1,82 @@
+package ch11.after;
+
+public class Shape {
+    public static final int TYPECODE_LINE = 0;
+    public static final int TYPECODE_RECTANGLE = 1;
+    public static final int TYPECODE_OVAL = 2;
+
+    private final int _typeCode;
+    private final int _startx;
+    private final int _starty;
+    private final int _endx;
+    private final int _endy;
+
+    public static Shape create(int typeCode, int startx, int starty, int endx, int endy) {
+        return new Shape(typeCode, startx, starty, endx, endy);
+    }
+
+    private Shape(int typeCode,
+                 int startx,
+                 int starty,
+                 int endx,
+                 int endy) {
+        _typeCode = typeCode;
+        _startx = startx;
+        _starty = starty;
+        _endx = endx;
+        _endy = endy;
+    }
+
+    public int getTypeCode() {
+        return _typeCode;
+    }
+
+    public String getName() {
+        switch (_typeCode) {
+            case TYPECODE_LINE:
+                return "LINE";
+            case TYPECODE_RECTANGLE:
+                return "RECTANGLE";
+            case TYPECODE_OVAL:
+                return "OVAL";
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getName() +
+                "( _startx=" + _startx +
+                ", _starty=" + _starty +
+                ", _endx=" + _endx +
+                ", _endy=" + _endy +
+                ")]";
+    }
+
+    public void draw() {
+        switch (_typeCode) {
+            case TYPECODE_LINE:
+                drawLine();
+                break;
+            case TYPECODE_RECTANGLE:
+                drawRectangle();
+                break;
+            case TYPECODE_OVAL:
+                drawOval();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void drawLine() {
+        System.out.println("drawLine" + this.toString());
+    }
+    private void drawRectangle() {
+        System.out.println("drawRectangle" + this.toString());
+    }
+    private void drawOval() {
+        System.out.println("drawOval" + this.toString());
+    }
+}
