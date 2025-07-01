d17ca1179cc4fe90f87154d5fcdf801efa2916bf
thisisnew <thisisnew@naver.com>
2025-07-01 21:14:19 +0900

    ch13 리팩토링 전

---
+++ refactoring-java/src/main/java/ch13/before/Dice.java
@@ -0,0 +1,48 @@
+package ch13.before;
+
+import java.util.Random;
+
+public class Dice extends Random {
+    public Dice() {
+        super(314159L);
+    }
+
+    public Dice(long seed) {
+        super(seed);
+    }
+
+    @Override
+    public int nextInt() {
+        return nextInt(6) + 1;
+    }
+
+    @Override
+    public void nextBytes(byte[] bytes) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public long nextLong() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean nextBoolean() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public float nextFloat() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public double nextDouble() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public double nextGaussian() {
+        throw new UnsupportedOperationException();
+    }
+}
