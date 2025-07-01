92c063505cdaee0d4e734c638e0a83714ed33bf9
thisisnew <thisisnew@naver.com>
2025-07-01 21:16:45 +0900

    ch13 첫번째 리팩토링

---
+++ refactoring-java/src/main/java/ch13/after/Dice.java
@@ -0,0 +1,24 @@
+package ch13.after;
+
+import java.util.Random;
+
+public class Dice {
+
+    private final Random _random;
+
+    public Dice() {
+        _random = new Random(314159L);
+    }
+
+    public Dice(long seed) {
+        _random =  new Random(seed);
+    }
+
+    public int nextInt() {
+        return _random.nextInt(6) + 1;
+    }
+
+    public void setSeed(long seed) {
+        _random.setSeed(seed);
+    }
+}
